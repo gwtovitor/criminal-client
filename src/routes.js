d708318b57c41b733593ac41070cece0c4a5f9a6
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2023-04-14 16:30:36 -0300

    inicando feed

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,11 @@ import Loginfa from './Pages/Loginfa/loginfa'
 import Signfa from './Pages/Signfa/signfa'
 import Signcriador from './Pages/Signcriador/signcriador'
 import DadosBancarios from './Pages/Signcriador/dadosbancarios'
+import Feed from './Pages/Feed/feed'
+
+
 function RoutesApp(){
+
     return(
         <BrowserRouter>
             <Routes>
@@ -14,7 +18,8 @@ function RoutesApp(){
                 <Route path='/loginfa' element={<Loginfa/>}/>  
                 <Route path='/signfa' element={<Signfa/>}/>    
                 <Route path='/signcriador' element={<Signcriador/>}/>      
-                <Route path='/signcriador/dados' element={<DadosBancarios/>}/>                   
+                <Route path='/signcriador/dados' element={<DadosBancarios/>}/>        
+                <Route path='/feed' element={<Feed/>}/>                
             </Routes>
         </BrowserRouter>
     )
