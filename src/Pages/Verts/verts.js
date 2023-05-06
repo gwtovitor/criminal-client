86fb67dc96dfb41b9d064aa8aca33f384da135d4
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2023-05-06 13:59:14 -0300

    UPDATE VERTS + FEED

--- src/Pages/Verts/verts.js
+++ src/Pages/Verts/verts.js
@@ -7,7 +7,7 @@ import {
   faVolumeMute,
   faHeart,
   faPause,
-
+  faEllipsisVertical
 } from "@fortawesome/free-solid-svg-icons";
 import vocesabia from './Images/vocesabia.jpg'
 import luiz from './Images/luiz.jpg'
@@ -17,7 +17,7 @@ import jr from './Images/junior.jpg'
 import { Image } from "react-bootstrap";
 
 function Verts() {
-  
+
   const [hovering, setHovering] = useState(false);
   const [playing, setPlaying] = useState(false);
   const videoRef = useRef([]);
@@ -43,11 +43,11 @@ function Verts() {
   };
 
   const handleMuteClick = (index) => {
-  const newIsMuted = [...isMuted];
-  newIsMuted[index] = !newIsMuted[index];
-  setIsMuted(newIsMuted);
-  const video = videoRefs[index].current;
-  video.muted = !video.muted;
+    const newIsMuted = [...isMuted];
+    newIsMuted[index] = !newIsMuted[index];
+    setIsMuted(newIsMuted);
+    const video = videoRefs[index].current;
+    video.muted = !video.muted;
   };
 
   const handleLikeClick = (index) => {
@@ -63,11 +63,12 @@ function Verts() {
       return newVideos;
     });
   };
-  
+
 
 
   const [video, setVideos] = useState([
-    { user: 'gwtovitor_',
+    {
+      user: 'gwtovitor_',
       name: 'Vitor Augusto',
       src: 'https://samplelib.com/lib/preview/mp4/sample-5s.mp4',
       isPlaying: false,
@@ -78,7 +79,8 @@ function Verts() {
       picture: vitor,
       legenda: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
     },
-    { user: 'lidiabzz_',
+    {
+      user: 'lidiabzz_',
       name: 'Lidia Beatriz',
       src: 'https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4',
       isMuted: false,
@@ -88,7 +90,8 @@ function Verts() {
       picture: lidia,
       legenda: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
     },
-    { user: 'user123_',
+    {
+      user: 'user123_',
       name: 'User Name',
       src: 'https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4',
       isMuted: false,
@@ -97,9 +100,10 @@ function Verts() {
       likes: 15,
       picture: luiz,
       legenda: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
-     
+
     },
-    { user: 'user123_',
+    {
+      user: 'user123_',
       name: 'User Name',
       src: 'https://assets.mixkit.co/videos/preview/mixkit-red-sports-car-74-large.mp4',
       isMuted: false,
@@ -118,50 +122,60 @@ function Verts() {
 
 
 
-    return (
+  return (
 
     <div>
       {video.map((src, index) => (
-       
-         
+
+        
         <div className="verts-player-container" key={index}>
-        <div className="userdata">
-            <Image className="profile-picture-verts" src={src.picture}/>
-           <div style={{display: 'flex', flexDirection:'column'}}>
-              <span style={{color: 'white'}}>{src.name}</span>
-              <span style={{fontSize: '10px', color: 'white'}}>{src.user}</span>
-           </div>
-            </div>
+          <div className="overlay" 
+            onMouseEnter={handleMouseEnter}
+            onMouseOut={handleMouseLeave} 
+            onClick={() => handleVideoClick(index)}>
+            <p>CC</p>
+          </div>
           <video
-           onMouseEnter={handleMouseEnter}
-           onMouseOut={handleMouseLeave}
+            onMouseEnter={handleMouseEnter}
+            onMouseOut={handleMouseLeave}
             className="verts-player"
             src={src.src}
             muted={src.isMuted}
             onClick={() => handleVideoClick(index)}
             ref={videoRefs[index]}
           />
+       
           {hovering && (
             <div className="play-button-verts">
               <button className="verts-play-pause-button">
-                <FontAwesomeIcon icon={playing ? faPause : faPlay} />
+                <FontAwesomeIcon  icon={playing ? '' : faPlay} />
               </button>
             </div>
           )}
           <div className="verts-controls">
-          <button className="verts-mute-button">
-              <FontAwesomeIcon className="control-buttons" onClick={() => handleLikeClick(index)} style={{ color: src.isLiked ? 'red' : 'black'}} icon={faHeart} />
-              <FontAwesomeIcon className="control-buttonsmobile" onClick={() => handleLikeClick(index)} style={{ color: src.isLiked ? 'red' : 'white'}} icon={faHeart} />
+           
+          </div>
+
+          <div className="verts-controls">
+            <button className="verts-mute-button">
+            <Image className="profile-picture-verts" src={src.picture} />
+              <FontAwesomeIcon className="control-buttons" onClick={() => handleLikeClick(index)} style={{ color: src.isLiked ? 'red' : 'black', fontSize:'20px'  }} icon={faHeart} />
+              <FontAwesomeIcon className="control-buttonsmobile" onClick={() => handleLikeClick(index)} style={{ color: src.isLiked ? 'red' : 'white', fontSize:'20px'  }} icon={faHeart} />
+          
             </button>
             <text className="text-likes">{src.likes}</text>
-            
+
             <button className="verts-mute-button">
-              <FontAwesomeIcon onClick={() => handleMuteClick(index)}  className="control-buttons" style={{ color: "black" }} icon={isMuted[index] ? faVolumeMute : faVolumeUp} />
-              <FontAwesomeIcon onClick={() => handleMuteClick(index)}  className="control-buttonsmobile" style={{ color: "white" }} icon={isMuted[index] ? faVolumeMute : faVolumeUp} />
+              <FontAwesomeIcon onClick={() => handleMuteClick(index)} className="control-buttons" style={{ color: "black", fontSize:'20px'  }} icon={isMuted[index] ? faVolumeMute : faVolumeUp} />
+              <FontAwesomeIcon onClick={() => handleMuteClick(index)} className="control-buttonsmobile" style={{ color: "white",fontSize:'20px'  }} icon={isMuted[index] ? faVolumeMute : faVolumeUp} />
+                 
             </button>
-            
-         
+            <button className="verts-mute-button">
+            <FontAwesomeIcon className="control-buttons" style={{ color: 'black'  }} icon={faEllipsisVertical} />
+            <FontAwesomeIcon className="control-buttonsmobile" style={{ color: 'white', fontSize:'20px', marginTop:'-10px' }} icon={faEllipsisVertical} />
+            </button>
           </div>
+          <span className="username-verts">@{src.user}</span>
           <span className="legenda-verts">{src.legenda}</span>
         </div>
 
