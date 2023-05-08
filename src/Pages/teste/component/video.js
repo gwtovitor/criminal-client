1912de8c958288c1436b50142eb562f3f5be731a
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2023-05-08 10:10:42 -0300

    up

---
+++ src/Pages/teste/component/video.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+function Video(){
+    return <div className="video">
+        <h1>Helow from video</h1>
+    </div>
+}
+export default Video;
--- src/Pages/teste/index.js
+++ src/Pages/teste/index.js
@@ -1,37 +1,10 @@
-import React, { useState } from "react";
+import './app.css'
+import React from 'react'
+import Video from './component/video'
 
-function Teste({ onSwipeUp }) {
-  const [startY, setStartY] = useState(0);
-  const [isSwiping, setIsSwiping] = useState(false);
 
-  const handleTouchStart = (e) => {
-    setStartY(e.touches[0].clientY);
-    setIsSwiping(true);
-  };
-
-  const handleTouchMove = (e) => {
-    const currentY = e.touches[0].clientY;
-    const distance = currentY - startY;
-
-    if (distance > 100 && isSwiping) {
-      onSwipeUp();
-      setIsSwiping(false);
-    }
-  };
-
-  const handleTouchEnd = () => {
-    setIsSwiping(false);
-  };
-
+export default function Teste(){
   return (
-    <div
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
-      Swipe up to continue
-    </div>
-  );
+    <div className='App'><div className='app__video'><Video/></div></div>
+  )
 }
-
-export default Teste;
