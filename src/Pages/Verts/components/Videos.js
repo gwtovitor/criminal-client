0d3c977c3f21bf81a60741b11fee48cd4d292fa4
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2023-05-08 19:01:27 -0300

    up verts

--- src/Pages/Verts/components/Videos.js
+++ src/Pages/Verts/components/Videos.js
@@ -2,10 +2,11 @@ import { MoreVert } from "@mui/icons-material";
 import React, { useRef, useState } from "react";
 import { ThumbUp } from "@mui/icons-material";
 import "./Video.css";
-import { ArrowBack } from "@mui/icons-material";
 import { Avatar } from "@mui/material";
-
-function Videos({
+import { VolumeOff, VolumeUp } from "@mui/icons-material";
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+function Video({
   id,
   src,
   channel,
@@ -14,25 +15,49 @@ function Videos({
   avatar
 }) {
   const [playing, setPlaying] = useState(false);
-  const [subs, setSubs] = useState(false);
-
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [muted, setMuted] = useState(false);
   const videoRef = useRef(null);
+
   const handleVideoPress = () => {
     if (playing) {
       setPlaying(false);
       videoRef.current.pause();
     } else {
       videoRef.current.play();
-      setPlaying((play) => !play);
+      setPlaying(true);
     }
   };
+  const handleMute = () => {
+    videoRef.current.muted = !muted;
+    setMuted((m) => !m);
+  };
 
-  const handleSubscribe = () => {
-    setSubs((sub) => !sub);
+  const scrollUp = () => {
+    window.scrollBy({
+      top: -100,
+      behavior: "smooth"
+    });
+  };
+
+  const scrollDown = () => {
+    window.scrollBy({
+      top: 100,
+      behavior: "smooth"
+    });
   };
 
   return (
     <div className="video">
+
+      <div className="video__buttons">
+        <button type="button" onClick={scrollUp} class="btn btn-primary btn-circle btn-xl"><KeyboardArrowUpIcon/>
+        </button>
+        <button type="button" onClick={scrollDown} class="btn btn-primary btn-circle btn-xl"><KeyboardArrowDownIcon/>
+        </button>
+
+      </div>
+
       <video
         id={id}
         className="video__player"
@@ -44,34 +69,25 @@ function Videos({
 
       <div className="shortsContainer">
         <div className="shortsVideoTop">
-          <div className="shortsVideoTopIcon">
-            <ArrowBack />
-          </div>
-          <div className="shortsVideoTopIcon">
-            <MoreVert />
-          </div>
+          <div className="shortsVideoTopIcon"></div>
         </div>
         <div className="shortsVideoSideIcons">
           <div className="shortsVideoSideIcon">
-            <ThumbUp />
-            <p>{like}</p>
-          </div>
+            <Avatar className="buttonsShortsSide" src={avatar} />
+            <ThumbUp className="buttonsShortsSide" />
 
+            <p>{like}</p>
+
+          </div>
+          <div onClick={handleMute}>
+            {muted ? <VolumeOff className="buttonsShortsSide" /> : <VolumeUp className="buttonsShortsSide" />}
+          </div>
+          <MoreVert style={{ marginTop: '15px' }} className="buttonsShortsSide" />
         </div>
         <div className="shortsBottom">
           <div className="shortsDesc">
-          
-                  <p className="description">{description}</p>
-          
-          </div>
-          <div className="shortDetails">
-            <Avatar
-              src={
-               {avatar}
-              }
-            />
-            <p>{channel}</p>
-     
+            <p className="description">{description}</p>
+
           </div>
         </div>
       </div>
@@ -79,4 +95,4 @@ function Videos({
   );
 }
 
-export default Videos;
+export default Video;
