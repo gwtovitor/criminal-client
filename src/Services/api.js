9fa6c92741e8f0f28cf5266dc2e289ab6e289552
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2023-06-21 23:09:45 -0300

    up

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://18.116.28.105:3333/'
-  //  baseURL: 'http://192.168.0.103:3333/'
-  baseURL: 'http://54.211.123.102:3333'
+   baseURL: 'http://192.168.0.103:3333/'
+//  baseURL: 'http://54.211.123.102:3333'
 });
 
 export default api;
